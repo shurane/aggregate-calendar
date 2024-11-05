3631ae742fa6f269ec933d9ad77bfba88c4e8f7b
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2024-11-05 12:09:50 -0800

    scrape both Fremont and Poplar gyms and log only subset of the event info

--- scrape.ts
+++ scrape.ts
@@ -17,34 +17,62 @@ const startDT = Temporal.Now.zonedDateTimeISO("UTC").startOfDay()
 const endDT = startDT
     .add(Temporal.Duration.from({ days: 5 }))
     .subtract(Temporal.Duration.from({ minutes: 1 }));
-// console.log(startDT.toInstant(), endDT.toInstant());
+console.log(startDT.toInstant(), endDT.toInstant());
 
 // info in https://boulderingproject.portal.approach.app/static/js/main.12354cbc.js
 // check "widgetsApiKey" and "Approach Portal"
-const calendarRequest = await fetch(
-    "https://widgets.api.prod.tilefive.com/cal?" +
-        new URLSearchParams({
-            // startDT: "2024-11-01T07:00:00.000Z",
-            // endDT: "2024-11-03T06:59:59.999Z",
-            startDT: startDT.toInstant().toString(),
-            endDT: endDT.toInstant().toString(),
-            locationId: "1",
-            activityId: "4,5,6",
-            page: "1",
-            pageSize: "50",
-        }),
-    {
-        headers: {
-            "Accept": "application/json, text/plain, */*",
-            "X-Api-Key": xApiKey,
-            "Authorization": "boulderingproject",
+
+// deno-lint-ignore no-explicit-any
+type CalendarData = Record<"calEvents" | "bookings", any[]>;
+const aggregateCalendarData: CalendarData = { calEvents: [], bookings: [] };
+
+const gyms = [
+    { id: "1", name: "Seattle Poplar" },
+    { id: "2", name: "Seattle Fremont" },
+];
+
+for (const { id } of gyms) {
+    const calendarRequest = await fetch(
+        "https://widgets.api.prod.tilefive.com/cal?" +
+            new URLSearchParams({
+                // startDT: "2024-11-01T07:00:00.000Z",
+                // endDT: "2024-11-03T06:59:59.999Z",
+                startDT: startDT.toInstant().toString(),
+                endDT: endDT.toInstant().toString(),
+                locationId: id,
+                activityId: "4,5,6",
+                page: "1",
+                pageSize: "50",
+            }),
+        {
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "X-Api-Key": xApiKey,
+                "Authorization": "boulderingproject",
+            },
+            method: "GET",
         },
-        method: "GET",
-    },
-);
-const calendarData = await calendarRequest.json();
-console.log(calendarData);
+    );
+    const calendarData = <CalendarData> await calendarRequest.json();
+    aggregateCalendarData.calEvents.push(...calendarData.calEvents);
+    aggregateCalendarData.bookings.push(...calendarData.bookings);
+}
+
+const bookingInfo = aggregateCalendarData.bookings.map((booking) => {
+    return {
+        id: booking.id,
+        name: booking.name,
+        uuid: booking.uuid,
+        description: booking.description,
+        startDT: booking.startDT,
+        endDT: booking.endDT,
+        duration: booking.duration,
+        locationId: booking.location.id,
+        locationName: booking.location.name,
+    };
+});
+console.log(bookingInfo);
 await Deno.writeTextFile(
     "calendar.json",
-    JSON.stringify(calendarData, null, 2),
+    JSON.stringify(aggregateCalendarData, null, 2),
 );
